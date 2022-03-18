7c09a13266e1222b51243b00bda08e60678b52f0
Jenn Robbins <Jennrobbins3@gmail.com>
2022-03-17 21:45:24 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(name, email){
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify({
+    name,
+    email
+})
+}
+return fetch("http://localhost:3000/users", configurationObject)
+.then(function (response){
+    return response.json();
+})
+.then(function (object){
+    document.body.innerHTML = object["id"]
+    
+})
+.catch(error => {
+    const body = document.querySelector('body');
+    const p = document.createElement('p');
+    p.textContent = error.message;
+    body.append(p)
+});
+}
+
+
